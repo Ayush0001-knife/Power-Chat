77a6085d467c2f55526e96d6d6f454c1056fcf69
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-22 20:00:00 +0530

    Power-chat

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-import { Container, Paper } from "@mui/material";
+import { Container, Paper, TextField, Typography, Button } from "@mui/material";
 
 const Login = () => {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
+  const toggleLogin = () => {
+    setIsLogin(!isLogin);
+  };
 
   return (
-    <Container component={"main"} maxWidth="sx">
+    <Container
+      component={"main"}
+      maxWidth="xs"
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Paper
         elevation={3}
         sx={{
@@ -15,7 +27,115 @@ const Login = () => {
           alignItems: "center",
         }}
       >
-        {isLogin ? <span>Login</span> : <span>Register</span>}
+        {isLogin ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{ marginTop: "1rem" }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Login
+              </Button>
+              <Typography
+                variant="body2"
+                align="center"
+                color="blue"
+                sx={{
+                  marginTop: "1rem",
+                  cursor: "pointer",
+                }}
+                onClick={toggleLogin}
+              >
+                Don't have an account? Register
+              </Typography>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Register</Typography>
+            <form
+              style={{
+                width: "100%",
+                marginTop: "1rem",
+              }}
+            >
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+              <Button
+                sx={{ marginTop: "1rem" }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Register
+              </Button>
+              <Typography
+                variant="body2"
+                align="center"
+                color="blue"
+                sx={{
+                  marginTop: "1rem",
+                  cursor: "pointer",
+                }}
+                onClick={toggleLogin}
+              >
+                Already have an account? Login
+              </Typography>
+            </form>
+          </>
+        )}
       </Paper>
     </Container>
   );
