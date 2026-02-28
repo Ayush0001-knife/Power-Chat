1364c6ee80e012748147ee79ec0975dffc28e426
Ayush Gautam <ayushstudy647@gmail.com>
2026-02-28 20:36:18 +0530

    first commit

---
+++ Frontend/src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Container, Paper } from "@mui/material";
+
+const Login = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  return (
+    <Container component={"main"} maxWidth="sx">
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? <span>Login</span> : <span>Register</span>}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
