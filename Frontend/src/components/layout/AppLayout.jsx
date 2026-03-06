839f5c6df9056f481e0b3f9859b357628f7d4cf9
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-06 18:31:53 +0530

    App Layout

--- Frontend/src/components/layout/AppLayout.jsx
+++ Frontend/src/components/layout/AppLayout.jsx
@@ -2,14 +2,51 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Title from "../shared/Title";
+import Grid from "@mui/material/Grid";
 
 const AppLayout = () => (WrappedComponent) => {
-  return (props) => {
+  return function Layout(props) {
     return (
       <>
         <Title title="My-Chat" subscription="Welcome to my app!" />
         <Header />
-        <WrappedComponent {...props} />
+
+        <Grid container sx={{ height: "calc(100vh - 4rem)" }}>
+          {/* Left Sidebar */}
+          <Grid
+            size={{ md: 3 }}
+            sx={{
+              display: { xs: "none", md: "block" },
+              height: "100%",
+            }}
+          >
+            First
+          </Grid>
+
+          {/* Chat Area */}
+          <Grid
+            size={{ xs: 12, sm: 8, md: 6 }}
+            sx={{
+              height: "100%",
+            }}
+          >
+            <WrappedComponent {...props} />
+          </Grid>
+
+          {/* Right Panel */}
+          <Grid
+            size={{ md: 3 }}
+            sx={{
+              display: { xs: "none", md: "block" },
+              height: "100%",
+              padding: "2rem",
+              bgcolor: "rgba(0,0,0,0.85)",
+            }}
+          >
+            Third
+          </Grid>
+        </Grid>
+
         <Footer />
       </>
     );
