342d927f16a5b74972fc69bf0c79254e09814cdc
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-06 18:38:10 +0530

    Header

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -1,7 +1,29 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { orange } from "../../constants/color";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box sx={{ flexGrow: 1, height: "4rem" }}>
+      <AppBar position="static" sx={{ bgcolor: orange }}>
+        <Toolbar>
+          <Typography
+            variant="h6"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Power Chat
+          </Typography>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "block" },
+            }}
+          >
+            <IconButton></IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
---
+++ Frontend/src/constants/color.js
@@ -0,0 +1,1 @@
+export const orange="#ff5722";
