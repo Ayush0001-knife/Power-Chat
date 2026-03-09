9a34bb81b9dc894df8c50aec9b4d9ce3245f3bcc
Ayush Gautam <ayushstudy647@gmail.com>
2026-03-09 20:00:00 +0530

    Home-Page-Responsivness

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -1,8 +1,11 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import { orange } from "../../constants/color";
+import { Menu as MenuIcon } from "@mui/icons-material";
 
 const Header = () => {
+  const handleMobile = () => {};
+
   return (
     <Box sx={{ flexGrow: 1, height: "4rem" }}>
       <AppBar position="static" sx={{ bgcolor: orange }}>
@@ -15,10 +18,12 @@ const Header = () => {
           </Typography>
           <Box
             sx={{
-              display: { xs: "none", sm: "block" },
+              display: { xs: "block", sm: "none" },
             }}
           >
-            <IconButton></IconButton>
+            <IconButton color="inherit" onClick={handleMobile}>
+              <MenuIcon></MenuIcon>
+            </IconButton>
           </Box>
         </Toolbar>
       </AppBar>
